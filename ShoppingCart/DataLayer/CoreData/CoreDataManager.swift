bf3ec5a3ba908fdbe23eb931d53062cb06d06a3f
mrott <rottmarius@gmail.com>
2018-02-01 13:30:27 +0200

    CoreData stack

---
+++ ShoppingCart/DataLayer/CoreData/CoreDataManager.swift
@@ -0,0 +1,79 @@
+//
+//  CoreDataManager.swift
+//  ShoppingCart
+//
+//  Created by Rott Marius Gabriel on 01/02/2018.
+//  Copyright © 2018 Rott Marius Gabriel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+fileprivate struct CoreDataManagerConstants {
+    static let modelName = "ShoppingCart"
+}
+
+final class CoreDataManager {
+    
+    private let modelName: String
+    
+    static let shared = CoreDataManager(modelName: CoreDataManagerConstants.modelName)
+    
+    private init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to Find Data Model")
+        }
+        
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to Load Data Model")
+        }
+        
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+        
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                                              configurationName: nil,
+                                                              at: persistentStoreURL,
+                                                              options: nil)
+        } catch {
+            fatalError("Unable to Load Persistent Store")
+        }
+        
+        return persistentStoreCoordinator
+    }()
+    
+    
+    func saveContext() {
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
